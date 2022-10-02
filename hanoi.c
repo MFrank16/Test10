2862d9c2c2f286355bf6fbc70f7e1ca50bc96319
MatusB <matus.bystricky@gmail.com>
2022-10-02 15:31:05 +0200

    preklad printov do slovenciny

--- hanoi.c
+++ hanoi.c
@@ -3,11 +3,11 @@
 void hanoi(int n, int a, int b)
 {
   if (n == 1) 
-    printf("move stone 1 from %d to %d\n", a, b);
+    printf("pohňi kameň 1 z %d do %d\n", a, b);
   else
   {
     hanoi(n - 1, a, 6 - a - b);
-    printf("move stone %d from %d to %d\n", n, a, b);
+    printf("pohny kam€n %d z %d ďo %d\n", n, a, b);
     hanoi(n - 1, 6 - a - b, b);
   }
 }
